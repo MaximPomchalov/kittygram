098c3a3045e4464ee3bab5bcb653d31fedb62277
Maxim Pomchalov <maxpo0443@gmail.com>
2025-04-27 18:39:18 +0300

    Viewsets and routers

--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,19 @@
-from django.urls import path
+from rest_framework.routers import DefaultRouter
 
-from cats.views import CatList, CatDetail
+from cats.views import CatViewSet
+from django.urls import include, path
 
+# Создаётся роутер
+router = DefaultRouter()
+# Вызываем метод .register с нужными параметрами
+router.register('cats', CatViewSet)
+# В роутере можно зарегистрировать любое количество пар "URL, viewset":
+# например
+# router.register('owners', OwnerViewSet)
+# Но нам это пока не нужно
 
 urlpatterns = [
-   path('cats/', CatList.as_view()),
-   path('cats/<int:pk>/', CatDetail.as_view()),
+    # Все зарегистрированные в router пути доступны в router.urls
+    # Включим их в головной urls.py
+    path('', include(router.urls)),
 ]
